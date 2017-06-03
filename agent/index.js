23eced10ab93ab33553ec2b743e3e2799ce71358
devend711 <devend711@gmail.com>
2017-06-03 11:22:49 -0400

    log response body

--- agent/index.js
+++ agent/index.js
@@ -1,16 +1,20 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
+// create application/json parser
+var jsonParser = bodyParser.json()
+
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.post('/', function(request, response) {
-  console.log('ping:', request.data);
+app.post('/', jsonParser, function(request, response) {
+  console.log(request.body);
   response.status(200);
   response.send('ping');
 });
