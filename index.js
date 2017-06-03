86fd54e328b956b06860fc9efbc8770f10250ac3
devend711 <devend711@gmail.com>
2017-06-03 12:06:55 -0400

    define http

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 app.set('port', (process.env.PORT || 5000));
