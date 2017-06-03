a66934ea6c32034ab86101a0097d8fa8b205c2e5
devend711 <devend711@gmail.com>
2017-06-03 13:19:37 -0400

    try different port

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var express = require('express');  
 var app = express();  
-var server = require('http').createServer(app);  
-var io = require('socket.io')(server);
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 const bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
 app.set('port', (process.env.PORT || 5000));
-server.listen(app.get('port'));  
 
 
 // create application/json parser
